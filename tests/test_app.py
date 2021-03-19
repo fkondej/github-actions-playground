7bef7b26775d9ef937b5225269ddb3076b086eb1
fkondej <fkondej@gmail.com>
2021-03-19 01:10:19 +0100

    add missing file

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+'''A module for testing'''
+import unittest
+
+
+class Tests(unittest.TestCase):
+
+    def test_example(self):
+        assert True
+
+
+if __name__ == "__main__":
+    unittest.main()
